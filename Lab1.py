c7a80575d15e0c32c6273aa6a0a14145cd45a4ad
133931212-myseneca <133059127+133931212-myseneca@users.noreply.github.com>
2023-06-13 20:12:31 -0400

    Update Lab1.py

--- Lab1.py
+++ Lab1.py
@@ -1,1 +1,16 @@
+import datetime
+
+def calculate_age():
+    current_year = datetime.datetime.now().year
+    try:
+        birth_year = int(input("Enter your birth year: "))
+        age = current_year - birth_year
+        if age < 0:
+            print("Invalid birth year! Please enter a valid year.")
+        else:
+            print("Your age is:", age)
+    except ValueError:
+        print("Invalid input! Please enter a valid year.")
+
+calculate_age()
 
